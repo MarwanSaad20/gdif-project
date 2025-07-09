d99c1eee7d9f5d8dc35995fa9d6a8de26597941c
Marwan <maroanasaaf0@gmail.com>
2025-07-09 19:30:37 +0300

    Auto commit at Wed 07/09/2025 19:30:37.66

--- data_intelligence_system/dashboard/callbacks/charts_callbacks.py
+++ data_intelligence_system/dashboard/callbacks/charts_callbacks.py
@@ -24,6 +24,7 @@ from data_intelligence_system.ml_models.regression.ridge_regression import Ridge
 from data_intelligence_system.ml_models.classification.logistic_regression import LogisticRegressionModel
 from data_intelligence_system.ml_models.classification.random_forest import RandomForestModel
 from data_intelligence_system.ml_models.classification.xgboost_classifier import XGBoostClassifierModel
+from data_intelligence_system.ml_models.clustering.dbscan import DBSCANClusteringModel
 from data_intelligence_system.utils.logger import get_logger
 
 import matplotlib.pyplot as plt
